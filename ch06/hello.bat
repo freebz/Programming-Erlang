5bcc6974552dea2e7ac77b790f064de1ead9fc26
FreeBz <freebz@hananet.net>
2018-06-24 16:18:47 +0900

    6장 프로그램 컴파일하고 실행하기

---
+++ ch06/hello.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\erl5.5.3\bin\erl.exe" -noshell -s hello start -s init stop
---
+++ ch06/hello.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+erl -noshell -pa /home/joe/2006/book/JAERANG/Book/code\
+             -s hello start -s init stop
