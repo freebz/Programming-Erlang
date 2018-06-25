9c798c80d2e5f0f9bdd83ec437ca04ac6893195c
FreeBz <freebz@hananet.net>
2018-06-26 01:05:37 +0900

    9장 병행 프로그램과 오류

---
+++ ch09/lib_misc.erl
@@ -0,0 +1,17 @@
+-module(lib_misc).
+-export([on_exit/2,keep_alive/2]).
+
+on_exit(Pid, Fun) ->
+    spawn(fun() ->
+		  process_flag(trap_exit, true),
+		  link(Pid),
+		  receive
+		      {'EXIT', Pid, Why} ->
+			  Fun(Why)
+		  end
+	  end).
+
+
+keep_alive(Name, Fun) ->
+    register(Name, Pid = spawn(Fun)),
+    on_exit(Pid, fun(_Why) -> keep_alive(Name, Fun) end).
